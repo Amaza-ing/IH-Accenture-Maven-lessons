8495b1bcb0792f24b1cdf2c2b3565da54b3c0b8b
Adrián Maza <>
2023-04-18 10:49:58 +0200

    w2d2 exceptions

---
+++ src/main/java/com/ironhack/w2/d2/DivideByZeroException.java
@@ -0,0 +1,7 @@
+package com.ironhack.w2.d2;
+
+public class DivideByZeroException extends Exception {
+    public DivideByZeroException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/ironhack/w2/d2/Main.java
@@ -0,0 +1,102 @@
+package com.ironhack.w2.d2;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        CHECKED EXCEPTIONS
+
+        readFile("myFile.txt");
+
+
+//        UNCHECKED EXCEPTIONS
+
+        String name = null;
+        printLength(name);
+
+
+//        THROWS
+
+        int[] sampleArray = null;
+        try {
+            printArray(sampleArray);
+        } catch (NullPointerException e) {
+            System.out.println("The array cannot be null");
+        }
+
+
+//        THROWING EXCEPTIONS
+
+        int a = 900000000;
+        int b = 400000000;
+
+        try {
+            int product = multiply(a, b);
+            System.out.println(product);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Using long version...");
+            long product = multiply((long) a,(long) b);
+            System.out.println(product);
+        }
+
+
+//        CREATE OUR OWN EXCEPTIONS
+
+        try {
+            int result = divide(123, 0);
+            System.out.println(result);
+        } catch (DivideByZeroException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        System.out.println("Ha llegado hasta aquí");
+    }
+
+    public static void readFile(String fileName){
+        try {
+            FileReader reader = new FileReader(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("The file does not exist");
+//            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Something happened");
+        }
+    }
+
+    public static void printLength(String myString) {
+        try {
+            System.out.println(myString.length());
+        } catch (NullPointerException e) {
+            System.out.println("String cannot be null");
+        }
+    }
+
+    public static void printArray(int[] numArray) throws NullPointerException {
+        for (int i = 0; i < numArray.length; i++) {
+            System.out.println(numArray[i]);
+        }
+    }
+
+    public static int multiply(int a, int b) {
+        int product = a * b;
+        if (product / a != b) {
+            throw new IllegalArgumentException("Product input is too great for int");
+        }
+        return product;
+    }
+
+    public static long multiply(long a, long b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) throws DivideByZeroException {
+        if (b == 0) {
+            throw new DivideByZeroException("You cannot divide by zero");
+        }
+        return a / b;
+    }
+}
